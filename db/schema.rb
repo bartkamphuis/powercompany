51803ecc4029faed26c317d7e5e89ac2559abdd4
Bart <bart.kamphuis@gmail.com>
2023-10-02 14:55:51 +1300

    postgres connection

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_02_001647) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -29,8 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_001647) do
 
   create_table "purchases", force: :cascade do |t|
     t.date "purchase_date"
-    t.integer "customer_id", null: false
-    t.integer "pack_id", null: false
+    t.bigint "customer_id", null: false
+    t.bigint "pack_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_purchases_on_customer_id"
